c4dfb680477803997e750a766948ff99157336ac
risalatulm614@gmail.com <risalatulm614@gmail.com>
2021-12-03 12:23:37 +0700

    commit pertama

---
+++ app/Http/Controllers/Api/ProdukController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Produk;
+
+class ProdukController extends Controller
+{
+    public function index(){
+        // dd($requset->all());die();
+        $produk = Produk::all();
+        return response()->json([
+            'success' => 1,
+            'message' => 'Get Produk Berhasil',
+            'produks' => $produk
+        ]);
+    }
+
+    public function delete(Request $request){
+        $product = Produk::where('id', $request->id)->first();
+        if ($product){
+            $product->delete();
+            return response()->json([
+                'success' => 0,
+                'message' => 'Produk berhasil dihapus' 
+            ]);
+        } else {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Produk tidak di temukan' 
+            ]);
+        }
+       
+    }
+
+    public function update(Request $request){
+        $product = Produk::where('id', $request->id)->first();
+        if ($product){
+            $product->update($request->all());
+            return response()->json([
+                'success' => 1,
+                'message' => 'Produk berhasil diubah',
+                'data' => $request->all() 
+            ]);
+        } else {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Produk tidak di temukan',
+                'data' => $request->all()  
+            ]);
+        }
+       
+    }
+}
